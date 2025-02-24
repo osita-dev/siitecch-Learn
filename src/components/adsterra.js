f7d18204a26a35c7c496bd01e88b8052eac9ee2f
Christopher osita <70627592+osita-dev@users.noreply.github.com>
2025-02-24 08:18:38 +0100

    Update adsterra.js

--- src/components/adsterra.js
+++ src/components/adsterra.js
@@ -2,33 +2,33 @@ import { useEffect } from "react";
 
 const AdsterraBanner = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = `
-      window.atOptions = {
-       'key' : '6a1e732fc686cc4710fbf36262fe2284',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
-      };
-    `;
-    document.body.appendChild(script);
+    // Create the iframe
+    const adFrame = document.createElement("iframe");
+    adFrame.src = `https://www.adsterra.com/script/6a1e732fc686cc4710fbf36262fe2284`;
+    adFrame.style.width = "100vw"; // Full width
+    adFrame.style.height = "50px";
+    adFrame.style.border = "none";
+    adFrame.style.position = "fixed";
+    adFrame.style.bottom = "0";
+    adFrame.style.left = "0";
+    adFrame.style.zIndex = "9999";
 
+    document.getElementById("adsterra-banner").appendChild(adFrame);
+
+    // Load the external Buttergem script
     const adScript = document.createElement("script");
-    adScript.type = "text/javascript";
-    adScript.async = true;
     adScript.src = "//buttergem.com/6a1e732fc686cc4710fbf36262fe2284/invoke.js";
+    adScript.async = true;
     document.body.appendChild(adScript);
 
+    // Cleanup on unmount
     return () => {
-      document.body.removeChild(script);
+      document.getElementById("adsterra-banner").innerHTML = "";
       document.body.removeChild(adScript);
     };
   }, []);
 
-  return <div id="adsterra-container" className="adsterra-container"></div>;
+  return <div id="adsterra-banner" style={{ width: "100%", position: "fixed", bottom: 0, left: 0, zIndex: 9999 }} />;
 };
 
 export default AdsterraBanner;
